a5afde2d2705159ba27194056df834a164f30d59
Dang Tuan Anh <tuananhblablo@gmail.com>
2020-02-06 11:29:09 +0700

    dcm

--- catch-object-game/src/game/util/entitiesFactory.js
+++ catch-object-game/src/game/util/entitiesFactory.js
@@ -5,9 +5,10 @@ export const createContainer = (world, x, y, tx, ty) => {
   world.addComponent(id, "position", new Vector2(x, y));
   world.addComponent(id, "target", new Vector2(tx, ty));
   world.addComponent(id, "speed", 200);
-  world.addComponent(id, "width", 10);
-  world.addComponent(id, "height", 5);
-  world.addComponent(id, "containter", true);
+  world.addComponent(id, "width", 50);
+  world.addComponent(id, "height", 50);
+  world.addComponent(id, "container", true);
+  return id;
 };
 
 export const createLoot = (world, x, y, tx, ty) => {
@@ -16,4 +17,6 @@ export const createLoot = (world, x, y, tx, ty) => {
   world.addComponent(id, "target", new Vector2(tx, ty));
   world.addComponent(id, "speed", 200);
   world.addComponent(id, "radius", 5);
+  world.addComponent(id, "loot", true);
+  return id;
 };
--- catch-object-game/src/index.js
+++ catch-object-game/src/index.js
@@ -1,54 +1,35 @@
+import createWorld from "./ecs";
 import "./index.css";
-import createWorld from "./ecs/index";
-// import resizeCanvas from "gdxjs/lib/resizeCanvas";
-// import createBatch from "gdxjs/lib/createBatch";
-// import createCamera from "gdxjs/lib/orthoCamera";
-// import loadTexture from "gdxjs/lib/loadTexture";
-// import createWhiteTex from "gl-white-texture";
-// import loadAtlas from "gdxjs/lib/loadAtlas";
+import createObject from "./game/system/createObjectSystem";
+import renderSystem from "./game/system/renderSystem";
 
-import renderContainer from "./game/system/renderSystem";
-import { createObject } from "./game/system/createObjectSystem";
+const canvas = document.getElementById("main");
+const context = canvas.getContext("2d");
 
-const init = async () => {
-  const info = document.getElementById("info");
-  const canvas = document.getElementById("main");
-  const context = canvas.getContext("2d");
+const world = createWorld();
+world.canvas = canvas;
+world.context = context;
+console.log(canvas.width, canvas.height);
+const imageBackground = new Image();
+imageBackground.src = "./yellow.jpg";
+const imageContainer = new Image();
+imageContainer.src = "./ellipse2.png";
 
-  const world = createWorld();
-  world.context = context;
-  world.canvas = canvas;
+createObject(world);
+renderSystem(world, imageContainer);
 
-  // const imagePlayer = await loadTexture(gl, "./smile.png");
-  const update = delta => {
-    world.setDelta(delta);
-    world.process();
-  };
-
-  const imageBackground = new Image();
-  imageBackground.src = "./yellow.jpg";
-  const imageContainer = new Image();
-  imageContainer.src = "./ellipse.png";
-
-  renderContainer(world, imageContainer);
-  createObject(world);
-
-  let lastTime = Date.now();
-  let fps = 0;
-
-  (function loop() {
-    const delta = Date.now() - lastTime;
-    lastTime = Date.now();
-    fps = Math.floor(1000 / delta);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(imageBackground, 0, 0, canvas.width, canvas.height);
-    update(delta / 1000);
-    requestAnimationFrame(loop);
-  })();
-
-  //   setInterval(() => {
-  //     info.innerHTML = `FPS: ${fps}`;
-  //   }, 1000);
+const update = delta => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(imageBackground, 0, 0, canvas.width, canvas.height);
+  world.setDelta(delta);
+  world.process();
 };
 
-init();
+let lastUpdate = Date.now();
+(function loop() {
+  const delta = Date.now() - lastUpdate;
+  lastUpdate = Date.now();
+  update(delta / 1000);
+
+  requestAnimationFrame(loop);
+})();
